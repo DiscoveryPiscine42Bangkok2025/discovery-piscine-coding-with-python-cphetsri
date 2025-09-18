abb05368c89bfb35d96173bd7e847d184f38dee4
cphetsri <68010233@kmitl.ac.th>
2025-09-18 10:53:14 +0000

    done with password in cell02 ex02

---
+++ cell02/ex02/password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+    inpass = input()
+    password = "Python is awesome"
+
+    if password == inpass:
+        print("ACCESS GRANTED")
+
+    else:
+        print("ACCESS DENIED")
+
+main()

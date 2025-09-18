cbacc7b309fd1695f4738763fb48a7e088ae740d
cphetsri <68010233@kmitl.ac.th>
2025-09-18 09:25:09 +0000

    I just have known that this discovery has a git so i push all of work that i'm finished

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,19 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whatsyourname.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chaphets <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/09/17 20:43:43 by chaphets          #+#    #+#              #
+#    Updated: 2025/09/17 21:22:01 by chaphets         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def main():
+    first_name = input("Hey, what's your first name? : ")
+    last_name = input("And your last name? : ")
+
+    print("Well, pleased to meet you, " + first_name, last_name)
+
+main()

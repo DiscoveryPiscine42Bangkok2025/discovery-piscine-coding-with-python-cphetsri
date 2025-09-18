7a9f7f79e045f20395c10be655e1fb0fde2de83b
cphetsri <68010233@kmitl.ac.th>
2025-09-18 09:39:10 +0000

    new rush folder

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    iszero.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chaphets <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/09/17 21:29:05 by chaphets          #+#    #+#              #
+#    Updated: 2025/09/17 22:02:12 by chaphets         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def main():
+    num = input()
+
+    if num == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+
+main()

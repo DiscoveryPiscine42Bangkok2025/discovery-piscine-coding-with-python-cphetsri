632a42c0727129d178cd1ae421c2725ae351f797
cphetsri <68010233@kmitl.ac.th>
2025-09-19 11:38:08 +0000

    finish with the cell05 ex12

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("none")
+    else:
+        text = sys.argv[1]
+        result = "".join([ch for ch in text if ch == "z"])
+        if result:
+            print(result)
+        else:
+            print("none")
+
+main()

bb677c9c09a8fe45a30f2a591b7cf0059263e693
cphetsri <68010233@kmitl.ac.th>
2025-09-19 07:52:16 +0000

    done with cell04 ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    stnum = int(input("Give me the first number: "))
+    sdnum = int(input("Give me the second number: "))
+    print("Thank you!")
+    print(f"{stnum} + {sdnum} = {stnum + sdnum} ")
+    print(f"{stnum} - {sdnum} = {stnum - sdnum} ")
+    print(f"{stnum} / {sdnum} = {int(stnum / sdnum)} ")
+    print(f"{stnum} * {sdnum} = {stnum * sdnum} ")
+main()
